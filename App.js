7bba5fe061d620fea6881d3e4c48e28cfd4bb2de
Erick Jair <velforver@outlook.com>
2023-10-01 21:32:41 -0300

    calculadora com erro de digitação

--- App.js
+++ App.js
@@ -117,7 +117,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'darkblue',
+    backgroundColor: 'blue',
   },
   results: {
     flex: 2,
